5307debd2403855f9e28b0515097c8d505148c4b
Dougy <dougycloud@gmail.com>
2022-08-01 15:34:05 +0200

    add event listeners click start and instructions, seperated css files, add multiples screens

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+const btnStart = document.getElementById('start');
+const btnRules = document.getElementById('instructions');
+const middleBox = document.getElementById('middle-box');
 
+// SCREENS
+
+const youWin = document.querySelectorAll(".you-win")
+const youLose = document.querySelectorAll(".you-lose")
+const gameRules = document.querySelectorAll(".game-rules")
+const shazamMaster = document.querySelectorAll(".shazam-master")
+const showerShuffler = document.querySelectorAll(".shower-shuffler")
+
+
+
+// loading page
+window.addEventListener('load', (event) => {
+for (let element of youWin){
+        element.style.display="none";
+}
+for (let element of youLose){
+    element.style.display="none";
+}
+for (let element of shazamMaster){
+    element.style.display="none";
+}
+for (let element of showerShuffler){
+    element.style.display="none";
+}
+for (let element of gameRules){
+    element.style.display="none";
+}
+});
+
+btnRules.addEventListener('click', () => {
+    for (let element of gameRules){
+        element.style.display="flex";
+    }
+});
+
+btnStart.addEventListener('click', () => {
+    for (let element of showerShuffler){
+        element.style.display="flex";
+    };
+});
+
+
+    
