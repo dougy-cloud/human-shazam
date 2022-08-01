d9baad6d0f6668f6e3af7335a2c496e1009a1233
Dougy <dougycloud@gmail.com>
2022-08-01 23:15:16 +0200

    shazam.js added with clips and clips by level

---
+++ shazam.js
@@ -0,0 +1,354 @@
+let bohemianRhapsody = new Audio('/music-clips/clips-lvl-1/Bohemian Rhapsody - Queen.mp3')
+let myHeartWill = new Audio('/music-clips/clips-lvl-1/Celine Dion - My Heart Will Always Go On.mp3')
+let loseYourself = new Audio('/music-clips/clips-lvl-1/Eminem - Lose Yourself.mp3')
+let americanBoy = new Audio('/music-clips/clips-lvl-1/Estelle - American Boy.mp3')
+let crazy = new Audio('/music-clips/clips-lvl-1/Gnarls Barkley - Crazy.mp3')
+let hallelujah = new Audio('/music-clips/clips-lvl-1/Jeff Buckley - Hallelujah.mp3')
+let runningUp = new Audio('/music-clips/clips-lvl-1/Kate Bush - Running Up That Hill.mp3')
+let pokerFace = new Audio('/music-clips/clips-lvl-1/Lady Gaga - Poker Face.mp3')
+let theEarthSong = new Audio ('/music-clips/clips-lvl-1/Nina Simone - I Put a Spell On You.mp3')
+let kiss = new Audio('/music-clips/clips-lvl-1/Prince - Kiss.mp3')
+let letsGetIt = new Audio("/music-clips/clips-lvl-1/The Black Eyed Peas - Let's Get It Started.mp3")
+let theEnd = new Audio('/music-clips/clips-lvl-1/The Doors - The End.mp3')
+let messageInABottle = new Audio('/music-clips/clips-lvl-1/The Police - Message in a Bottle.mp3')
+let iWannaDance = new Audio('/music-clips/clips-lvl-1/Whitney Houston - I Wanna Dance With Somebody.mp3')
+
+let levelOneclips = [
+    {
+        name: 'Queen - Bohemian Rhapsody',
+        answerA: '',
+        answerB: 'Queen - Bohemian Rhapsody',
+        answerC: '',
+        clip: bohemianRhapsody,
+
+    }, 
+    {
+        name: 'Céline Dion - My Heart Will Go On',
+        answerA: '',
+        answerB: '',
+        answerC: 'Céline Dion - My Heart Will Go On',
+        clip: myHeartWill,
+    },
+    {
+        name: 'Eminem - Lose Yourself',
+        answerA: 'Eminem - Till I collapse',
+        answerB: 'Eminem - Lose Yourself',
+        answerC: '',
+        clip: loseYourself,
+    },
+    {
+        name: 'Estelle - American Boy',
+        answerA: 'Kanye West - American Boy',
+        answerB: 'Estelle - American Boy',
+        answerC: 'Esther - American Boy',
+        clip: americanBoy,
+    },
+    {
+        name: 'Gnarls Barkley - Crazy',
+        answerA: 'Nils Barkley - Crazy',
+        answerB: 'Nils Barkley - I remember',
+        answerC: 'Gnarls Barkley - Crazy',
+        clip: crazy,
+    },
+    {
+        name: 'Jeff Buckley - Hallelujah',
+        answerA: 'Jeff Buckley - Hallelujah',
+        answerB: 'Shrek - Hallelujah',
+        answerC: 'Jeffrey Beckley - Hallelujah',
+        clip: hallelujah,
+    },
+    {
+        name: 'Kate Bush - Running Up That Hill',
+        answerA: 'Kate Bush - Running Up That Hill',
+        answerB: 'Stranger Things - Running Up That Hill',
+        answerC: 'Kate Bush - Moving',
+        clip: runningUp,
+    },
+    {
+        name: 'Lady Gaga - Poker Face',
+        answerA: '',
+        answerB: '',
+        answerC: 'Lady Gaga - Poker Face',
+        clip: pokerFace,
+    },
+    {
+        name: 'Michael Jackson - The Earth Song',
+        answerA: 'Michael Jackson - What About Us',
+        answerB: 'Michael Jackson - The Earth Song',
+        answerC: '',
+        clip: theEarthSong,
+    },
+    {
+        name: 'Prince - KISS',
+        answerA: 'Prince - KISS',
+        answerB: '',
+        answerC: '',
+        clip: kiss,
+    },
+    {
+        name: "The Black Eyed Peas - Let's Get It Started",
+        answerA: "The Black Eyed Peas - PUMP IT",
+        answerB: "The Black Eyed Peas - Let's Get It Started",
+        answerC: "The Black Eyed Peas - ", // 
+        clip: letsGetIt,
+    },
+    {
+        name: 'The Doors - The End',
+        answerA: 'Velvet Underground - Venus In Furs',
+        answerB: 'Velvet Underground - Sunday Morning',
+        answerC: 'The Doors - The End',
+        clip: theEnd,
+    },
+    {
+        name: 'The Police - Message In A Bottle',
+        answerA: '',
+        answerB: '',
+        answerC: 'The Police - Message In A Bottle',
+        clip: messageInABottle,
+    },
+    {
+        name: 'Whitney Houston - I Wanna Dance With Somebody',
+        answerA: '',
+        answerB: '',
+        answerC: 'Whitney House - I Wanna Dance With Somebody',
+        clip: iWannaDance,
+    }]
+
+let pimp = new Audio('/music-clips/clips-lvl-2/50 cent - P.I.M.P..mp3')
+let noOne = new Audio('/music-clips/clips-lvl-2/Alicia Keys - No One.mp3')
+let badGuy = new Audio('/music-clips/clips-lvl-2/Billie Eilish - bad guy.mp3')
+let preludes = new Audio('/music-clips/clips-lvl-2/Chopin - Prelude in E Minor op 28 no 4.mp3')
+let summertime = new Audio('/music-clips/clips-lvl-2/Ella Fitzgerald and Louis Armstrong - Summertime.mp3')
+let killingMeSoftly = new Audio('/music-clips/clips-lvl-2/Fugees - Killing Me Softly with His Song.mp3')
+let madAboutYou = new Audio('/music-clips/clips-lvl-2/Hooverphonic - Mad about you.mp3')
+let blueLights = new Audio('/music-clips/clips-lvl-2/Jorja Smith - Blue Lights.mp3')
+let videoGames = new Audio('/music-clips/clips-lvl-2/Lana Del Rey - Video Games.mp3')
+let walkOnTheWildSide = new Audio('/music-clips/clips-lvl-2/Lou Reed - Walk on the Wild Side.mp3')
+let killingInTheNameOf = new Audio('/music-clips/clips-lvl-2/Rage Against The Machine - Killing In the Name.mp3')
+let psychoKiller = new Audio('/music-clips/clips-lvl-2/Talking Heads - Psycho Killer.mp3')
+let everybodySGotToLeanSometime = new Audio("/music-clips/clips-lvl-2/The Korgis - Everybody's Got To Learn Sometime.mp3")
+let beMyBady = new Audio('/music-clips/clips-lvl-2/The Ronettes - Be My Baby.mp3')
+let timeOfTheSeason = new Audio('/music-clips/clips-lvl-2/The Zombies - Time Of The Season.mp3')
+let vitaminC = new Audio('/music-clips/clips-lvl-2/Vitamin C - Can.mp3')
+
+let levelTwoClips= [
+    {
+    name: 'PIMP',
+    artist: '50 cent',
+    answerA: '50 cent',
+    answerB: 'The Game',
+    answerC: 'Snoop Dogg',
+    clip: pimp,
+   },
+   {
+    name: 'No One',
+    artist: 'Alicia Keys',
+    answerA: 'Mariah Carey',
+    answerB: 'Alicia Keys',
+    answerC: 'Whitney House',
+    clip: noOne,
+   },
+   {
+    name: 'Bad Guy',
+    artist: 'Bilie Eilish',
+    answerA: '',
+    answerB: '',
+    answerC: 'Bilie Eilish',
+    clip: badGuy,
+   },
+   {
+    name: 'Prelude in E Minor, Op. 28, n°4',
+    artist: 'Chopin',
+    answerA: 'Chopin',
+    answerB: 'Beethoven',
+    answerC: 'Vivaldi',
+    clip: preludes,
+   },
+   {
+    name: 'Summertime',
+    artist: 'Ella Fitzgerald & Louis Armstrong',
+    answerA: 'Ella Fitzgerald & Louis Armstrong',
+    answerB: 'Aretha Franklin & Louis Armstrong',
+    answerC: 'Nina Simone & Louis Armstrong',
+    clip: summertime,
+   },
+   {
+    name: 'Killing Me Softly',
+    artist: 'The Fugees',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: killingMeSoftly,
+   },
+   {
+    name: 'Mad About You',
+    artist: 'Hooverphonic',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: madAboutYou,
+   },
+   {
+    name: 'Blue Lights',
+    artist: 'Jorja Smith',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: blueLights,
+   },
+   {
+    name: 'Video Games',
+    artist: 'Lana Del Rey',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: videoGames,
+   },
+   {
+    name: 'Walk On The Wild Side',
+    artist: 'Lou Reed',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: walkOnTheWildSide,
+   },
+   {
+    name: 'Killing In The Name Of',
+    artist: 'Rage Against The Machine',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: killingInTheNameOf,
+   },
+   {
+    name: 'Psycho Killer',
+    artist: 'Talking Heads',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: psychoKiller,
+   },
+   {
+    name: "Everybody's Gotta Learn Sometime",
+    artist: 'The Korgis',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: everybodySGotToLeanSometime,
+   },
+   {
+    name: 'Be My Baby',
+    artist: 'The Ronettes',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: beMyBady,
+   },
+   {
+    name: 'Time Of The Season',
+    artist: 'The Zombies',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: timeOfTheSeason,
+   },
+   {
+    name: 'Vitamin C',
+    artist: 'CAN',
+    answerA: '',
+    answerB: '',
+    answerC: 'Whitney House - I Wanna Dance With Somebody',
+    clip: vitaminC,
+   },
+]
+
+
+let sodade = new Audio('/music-clips/clips-lvl-3/Cesaria Evora -  Sodade.mp3')
+let laVieEnRose = new Audio('/music-clips/clips-lvl-3/Edith Piaf - La Vie En Rose.mp3')
+let bestOfYou = new Audio('/music-clips/clips-lvl-3/Foo Fighters - Best Of You.mp3')
+let heatWaves = new Audio('/music-clips/clips-lvl-3/Glass Animals - Heat Waves.mp3')
+let thisIlove = new Audio("/music-clips/clips-lvl-3/Guns N' Roses - This I Love.mp3")
+let timeInABottle = new Audio('/music-clips/clips-lvl-3/Jim Croce -Time In A Bottle.mp3')
+let noQuarter = new Audio('/music-clips/clips-lvl-3/Led Zeppelin - No Quarter.mp3')
+let youDontOwnMe = new Audio('/music-clips/clips-lvl-3/Lesley Gore - You Dont Own Me.mp3')
+let fadeIntoYou = new Audio('/music-clips/clips-lvl-3/Mazzy Star - Fade Into You.mp3')
+let pastimeParadise = new Audio('/music-clips/clips-lvl-3/Patti Smith - Pastime Paradise.mp3')
+let laBambola = new Audio('/music-clips/clips-lvl-3/Patty Pravo - La bambola.mp3')
+let theLessIKnowTheBetter = new Audio('/music-clips/clips-lvl-3/Tame Impala - The Less I Know The Better.mp3')
+let remember = new Audio('/music-clips/clips-lvl-3/The Shangri-Las - Remember (Walkin In The Sand).mp3')
+
+let levelThreeClips = [
+    {
+        name: "Sodade",
+        clip: sodade,
+    },
+    {
+        name: "La vie en rose",
+        clip: laVieEnRose,
+    },
+    {
+        name: "Best Of You",
+        clip: bestOfYou,
+    },
+    {
+        name: "This I Love",
+        clip: thisIlove,
+    },
+    {
+        name: "Time In A Bottle",
+        clip: timeInABottle,
+    },
+    {
+        name: "No Quarter",
+        clip: noQuarter,
+    },
+    {
+        name: "You Don't Own Me",
+        clip: youDontOwnMe,
+    },
+    {
+        name: "Fade Into You",
+        clip: fadeIntoYou,
+    },
+    {
+        name: "Pastime Paradise",
+        clip: pastimeParadise,
+    },
+    {
+        name: "La bambola",
+        clip: laBambola,
+    },
+    {
+        name: "The Less I Know The Better",
+        clip: theLessIKnowTheBetter,
+    },
+    {
+        name: "Remember",
+        clip: remember,
+    }
+]
+
+
+class levelOne {
+    constructor(name, answerA, answerB, answerC, clip) {
+        this.name = name;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.clip = clip;
+    }
+}
+
+class levelTwo extends levelOne {
+    constructor(name, artist, answerA, answerB, answerC, clip){
+        super(name, answerA, answerB, answerC, clip)
+        this.artist = artist;
+    }
+}
+
+class levelThree {
+    constructor(name, clip){
+        this.name = name;
+        this.clip = clip;
+    }
+}
